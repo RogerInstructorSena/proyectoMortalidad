dac46aa2ca880c0bc2f7721fcf4c389769ddd2e6
RogerInstructorSena <rslondono@misena.edu.co>
2020-04-20 18:04:16 -0500

    Configuraciones,Creación de Entities,Procedimiento Almacenado Departamento,Formulario Departamento

---
+++ proyectoMortalidad/Controllers/DepartamentosController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using proyectoMortalidad.Models;
+using proyectoMortalidad.Models.TableViewModels;
+
+
+namespace proyectoMortalidad.Controllers
+{
+    public class DepartamentosController : Controller
+
+    {
+        //Get: Departamentos
+        public ActionResult Index()
+        {
+            List<DepartamentosTVM> listaDepartamentos = null;
+            using (nofetal2017Entities db = new nofetal2017Entities())
+            {
+                listaDepartamentos = (from d in db.COD_DPTO_PREGUNTA
+                                     select new DepartamentosTVM
+                                     {
+                                         codDptoPreguntaId=Convert.ToInt16(d.codDptoPreguntaId),
+                                         codDptoPreguntaNombre = d.codPreguntaNombre
+                                     }).ToList();
+
+
+            }
+                return View(listaDepartamentos);
+        }
+
+    }
+}
---
+++ proyectoMortalidad/Models/TableViewModels/DepartamentosTVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace proyectoMortalidad.Models.TableViewModels
+{
+    public class DepartamentosTVM
+    {
+
+        public int? codDptoPreguntaId { get; set; }
+        public string codDptoPreguntaNombre { get; set; }
+    }
+
+}
